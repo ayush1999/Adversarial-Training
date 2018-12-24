112d81fd7cb3a6cbedb0434b907b62cfb40fe53a
ayush1999 <ayush.shridhar1999@gmail.com>
2018-12-24 13:04:02 +0530

    added more attacks

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+import foolbox
+import keras
 import numpy as np
-np.random.seed(2142)
-from keras.models import Input
-from keras.losses import mean_squared_error as scc
-from keras.utils import to_categorical
-from keras.datasets import mnist
 from keras.models import load_model
+from keras.datasets import mnist
 from keras import backend as K
 
-#load pretrained model
-model = load_model('mnist_resnet_mse_normalized.h5')
+K.set_learning_phase(0)
 
+model = load_model("mnist_resnet_mse.h5")
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_test = np.reshape(x_test, (10000, 1, 28, 28));
 
-x_test = np.reshape(x_test, (10000,1,28,28))/255
-y_test = to_categorical(y_test)
+# This works fine:
+model.predict(np.reshape(x_test[0], (1,1,28,28)))
+
+# foolbox code:
+fmodel = foolbox.models.KerasModel(model, bounds=(0,255))
+attack = foolbox.attacks.FGSM(fmodel)
+adv = attack(x_test[0], y_test[0])
 
 def get_ind(a):
     res = 0
@@ -22,21 +26,22 @@ def get_ind(a):
             res = i
     return res
 
-# Returns the adversarial image from x_test[i]
-def adversify(i):
-    c = x_test[i]
-    y = Input(shape=y_test[i].shape)
-    grads = K.gradients(scc(y, model.output), model.inputs)
-    func = K.function(model.inputs + [y, K.learning_phase()], grads)
-    
-    out = func([np.reshape(c, (1,1,28,28)), np.reshape(y_test[i], (1,10)), 0])
-    c += 0.05*np.sign(out[0][0])
-        
-    return c
+i = 0
+adv = attack(x_test[i], y_test[i])
+pred = model.predict(np.reshape(adv, (1,1,28,28)))
+print(get_ind(pred[0]))
+print(y_test[i])
 
-img = adversify(0)
+# Gradient attack
+attack1 = foolbox.attacks.LBFGSAttack(fmodel)
+adv1 = attack1(x_test[0], y_test[0])
+pred = model.predict(np.reshape(adv1, (1,1,28,28)))
+print(get_ind(pred[0]))
+print(y_test[i])
 
-if get_ind(model.predict(np.reshape(img, (1,1,28,28)))[0] )== get_ind(y_test[0]):
-    print("still gives correct output")
-else:
-    print("adversarial example works")
+# Gradient attack
+attack2 = foolbox.attacks.LBFGSAttack(fmodel)
+adv2 = attack2(x_test[0], y_test[0])
+pred = model.predict(np.reshape(adv2, (1,1,28,28)))
+print(get_ind(pred[0]))
+print(y_test[i])
